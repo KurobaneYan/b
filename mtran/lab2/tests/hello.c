e0ee3cff5385b1c9ffbfecd10d30d1f0df18530e
KurobaneYan <yan.kurobane@gmail.com>
2017-04-17 21:33:09 +0300

    Mtran add 2 lab, finished and passed

---
+++ mtran/lab2/tests/hello.c
@@ -0,0 +1,16 @@
+int main()
+{
+    char name = "Bob";
+    char name = "Matthew Zweig";
+    char aChar = 'A';
+    char bChar = '\n';
+    char cChar = '\t';
+    //char cChar = '\\';
+    char cChar = '\'';
+    char cChar = '/';
+    char cChar = '\"';
+    int age = 25;
+    printf("Hello %s\n\tyou are %d years old\n", name, age);
+    return 1;
+}
+
