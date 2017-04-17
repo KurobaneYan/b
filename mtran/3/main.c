84a74d29b05e060442c3637353722660a4740a1f
KurobaneYan <yan.kurobane@gmail.com>
2017-04-17 21:34:07 +0300

    Mtran add 3rd lab(finished)

---
+++ mtran/3/main.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+long calc_factorial(int n) {
+    if (n < 0) {
+        return -1;
+    } else if (n == 0) {
+        return 1;
+    }
+
+    return n * calc_factorial(n - 1);
+}
+
+int main() {
+    int n = 0;
+    int t;
+    char *c;
+
+    setlinebuf(stdout);
+
+    printf("how many times caculate factorial?");
+    scanf("%d", &n);
+
+    while(n) {
+        printf("enter number\n");
+        scanf("%d", &t);
+
+        if (t > 18) {
+            printf("factorial of %d is too big\n", t);
+        } else {
+            printf("factorial of %d is %ld\n", t, calc_factorial(t));
+        }
+        n--;
+    }
+    return 0;
+}
