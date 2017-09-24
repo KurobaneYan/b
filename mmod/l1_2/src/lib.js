5fd216f83c5095e18fbc408c931050f8ecf2db8b
KurobaneYan <yan.kurabane@gmail.com>
2017-09-24 23:59:03 +0300

    mmod l1 v1

--- mmod/l1_2/src/lib.js
+++ mmod/l1_2/src/lib.js
@@ -1,7 +1,7 @@
-const M = 676171
-const N = 100
+// const M = 676171
+// const N = 100
 
-function* gen1 (seed = 1994) {
+function * gen1 (seed = 1994) {
   // let seed = 1994
   while (true) {
     let sqr = seed ** 2
@@ -19,7 +19,7 @@ function* gen1 (seed = 1994) {
   }
 }
 
-function* gen2 (A = 1994, k = 271129, M = 676171) {
+function * gen2 (A = 1994, k = 271129, M = 676171) {
   // let A = 1994
   // const k = 271129
   while (true) {
@@ -28,6 +28,10 @@ function* gen2 (A = 1994, k = 271129, M = 676171) {
   }
 }
 
-const getK = (N) => 1 + Math.floor(Math.log2(N))
+const getK = N => 1 + Math.floor(Math.log2(N))
 
-export { gen1, gen2, getK }
+const getM = a => a.reduce((a, i) => a + i, 0) / a.length
+
+const getD = a => a.reduce((acc, i) => acc + (i ** 2 - getM(a) ** 2), 0) / a.length
+
+export { gen1, gen2, getK, getM, getD }
